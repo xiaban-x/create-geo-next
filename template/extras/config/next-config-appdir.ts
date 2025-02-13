c1faad52b5a3196fc814c15c9c71c8cd6813f169
xiaban <980892894@qq.com>
2025-02-13 18:26:27 +0800

    fix: 修复 next config 和 t3-oss/env 包的兼容性问题

--- template/extras/config/next-config-appdir.ts
+++ template/extras/config/next-config-appdir.ts
@@ -2,8 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
-import {NextConfig} from "next";
+import type {NextConfig} from "next";
 
 /** @type {import("next").NextConfig} */
 const config: NextConfig = {};
